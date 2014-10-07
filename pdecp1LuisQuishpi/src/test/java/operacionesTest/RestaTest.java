e9e8bbc0f4f2a7123b7ce5ec2090d2eea162e300
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-08 01:47:35 +0200

    Implementado RestaTest

--- pdecp1LuisQuishpi/src/test/java/operacionesTest/RestaTest.java
+++ pdecp1LuisQuishpi/src/test/java/operacionesTest/RestaTest.java
@@ -1,39 +1,42 @@
 package operacionesTest;
 
 import static org.junit.Assert.*;
+import operaciones.Resta;
 
 import org.junit.Test;
 
 public class RestaTest {
 
+    private Resta resta = new Resta(5, 1);
     @Test
     public void testCalcular() {
-        fail("Not yet implemented");
+        assertEquals(4, resta.calcular(), 10e-5);
     }
 
     @Test
     public void testGetNombre() {
-        fail("Not yet implemented");
+        assertEquals("-", resta.getNombre());
     }
 
     @Test
     public void testResta() {
-        fail("Not yet implemented");
+        assertEquals(5, resta.getValor1());
+        assertEquals(1, resta.getValor2());
     }
 
     @Test
     public void testGetValor1() {
-        fail("Not yet implemented");
+        assertEquals(5, resta.getValor1());
     }
 
     @Test
     public void testGetValor2() {
-        fail("Not yet implemented");
+        assertEquals(1, resta.getValor2());
     }
 
     @Test
     public void testToString() {
-        fail("Not yet implemented");
+        assertEquals("[5-1]", resta.toString());
     }
 
 }
