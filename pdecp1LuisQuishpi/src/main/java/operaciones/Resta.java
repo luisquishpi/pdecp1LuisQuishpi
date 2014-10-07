5ab42779a5c02f6d875274f4c632b30dc4f09809
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-08 01:48:13 +0200

    Implementado Clase Resta

---
+++ pdecp1LuisQuishpi/src/main/java/operaciones/Resta.java
@@ -0,0 +1,32 @@
+package operaciones;
+
+public class Resta extends Operaciones {
+
+    private int valor1;
+    private int valor2;
+    public Resta(int operator1, int operator2) {
+        this.valor1=operator1;
+        this.valor2=operator2;
+    }
+
+    public double calcular() {
+        return this.valor1-this.valor2;
+    }
+    public String getNombre(){
+        return "-";
+    }
+
+    public int getValor1() {
+        // TODO Auto-generated method stub
+        return this.valor1;
+    }
+
+    public int getValor2() {
+        // TODO Auto-generated method stub
+        return this.valor2;
+    }
+    @Override
+    public String toString() {
+        return "[" + this.valor1 + this.getNombre() + this.valor2 + "]";
+    }
+}
