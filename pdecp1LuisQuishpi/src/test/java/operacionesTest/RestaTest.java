a8fb2a2653dfb8cf6ab1c165195ec80189a9d636
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-08 01:41:21 +0200

    Creado RestaTest

---
+++ pdecp1LuisQuishpi/src/test/java/operacionesTest/RestaTest.java
@@ -0,0 +1,39 @@
+package operacionesTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RestaTest {
+
+    @Test
+    public void testCalcular() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetNombre() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testResta() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetValor1() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetValor2() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+}
