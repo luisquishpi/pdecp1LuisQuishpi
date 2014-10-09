0f5cfd7616ea0ded60b7284c88b5fddc0614bae5
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-10 00:31:28 +0200

    Creado VahiculoGestorTest

---
+++ pdecp1LuisQuishpi/src/test/java/vehiculosTest/VehiculoGestorTest.java
@@ -0,0 +1,29 @@
+package vehiculosTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VehiculoGestorTest {
+
+    @Test
+    public void testGetInstance() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAdd() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testPrecio() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+}
