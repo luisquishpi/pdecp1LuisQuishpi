39f261b33a5c81ea67324484309223866e0d34f3
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-09 22:12:07 +0200

    Creado y probado ProductoTest

---
+++ pdecp1LuisQuishpi/src/test/java/operacionesTest/ProductoTest.java
@@ -0,0 +1,36 @@
+package operacionesTest;
+
+import static org.junit.Assert.*;
+import operaciones.Producto;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProductoTest {
+
+    private Producto producto;
+    @Before
+    public void ini() {
+        producto = new Producto(6, 2);
+    }
+    @Test
+    public void testCalcular() {
+        assertEquals(4, producto.calcular());
+    }
+
+    @Test
+    public void testGetNombre() {
+        assertEquals("*", producto.getNombre());
+    }
+
+    @Test
+    public void testProducto() {
+        assertEquals(4, producto.calcular());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("[6*2]", producto.toString());
+    }
+
+}
