36652f52ff759316c45b7669b3e0a42fd82d4f3b
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-09 22:12:32 +0200

    Agregado Prodcuto en Main

--- pdecp1LuisQuishpi/src/main/java/operaciones/Main.java
+++ pdecp1LuisQuishpi/src/main/java/operaciones/Main.java
@@ -10,6 +10,7 @@ public class Main {
         operacionesColeccion.add(new Suma(5, 8));
         operacionesColeccion.add(new Resta(8, 4));
         operacionesColeccion.add(new Resta(9, 5));
+        operacionesColeccion.add(new Producto(2, 7));
 
         System.out.println("Total2:" + operacionesColeccion.total());
     }
