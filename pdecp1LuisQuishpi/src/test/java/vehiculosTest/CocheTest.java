7ef2cb41f161e6ce9063d394e27d9b468307a129
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-10 01:25:11 +0200

    Creado CocheTest

---
+++ pdecp1LuisQuishpi/src/test/java/vehiculosTest/CocheTest.java
@@ -0,0 +1,29 @@
+package vehiculosTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CocheTest {
+
+    @Test
+    public void testPrecio() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testToString() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCoche() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetCategoria() {
+        fail("Not yet implemented");
+    }
+
+}
