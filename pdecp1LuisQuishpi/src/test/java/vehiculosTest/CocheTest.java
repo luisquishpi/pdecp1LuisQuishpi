b7d5881dc403814989fc4ec760b2cf925b02fdac
Luis Quishpi <luisquishpi @138.4.205.235>
2014-10-10 01:33:18 +0200

    Actualizado CocheTest

--- pdecp1LuisQuishpi/src/test/java/vehiculosTest/CocheTest.java
+++ pdecp1LuisQuishpi/src/test/java/vehiculosTest/CocheTest.java
@@ -4,26 +4,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import vehiculos.Categorias;
+import vehiculos.Coche;
+import vehiculos.Vehiculo;
+
+import org.junit.Before;
+
 public class CocheTest {
+    private Vehiculo vehiculo;
+
+    @Before
+    public void before() {
+        vehiculo = new Coche(1, "Description", Categorias.A.categoria);
+    }
 
     @Test
     public void testPrecio() {
-        fail("Not yet implemented");
+        assertEquals(20.00, vehiculo.precio(2), 10e-5);
+        assertEquals(30.00, vehiculo.precio(3), 10e-5);
     }
 
     @Test
     public void testToString() {
-        fail("Not yet implemented");
+        assertEquals("Coche[Id: 1, Descripción: Description, Categoría: A]", vehiculo.toString());
     }
 
     @Test
     public void testCoche() {
-        fail("Not yet implemented");
+        assertEquals(30.00, vehiculo.precio(3), 10e-5);
     }
 
     @Test
     public void testGetCategoria() {
-        fail("Not yet implemented");
+        assertEquals(Categorias.A, ((Coche) vehiculo).getCategoria());
     }
 
 }
